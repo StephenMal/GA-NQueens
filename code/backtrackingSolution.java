192ccd021d99d87a8f597bdff2c38d140cb37d3c
Stephen <maldonado527@gmail.com>
2021-04-13 20:05:26 -0400

    Created the back tracking solution benchmark

--- code/backtrackingSolution.java
+++ code/backtrackingSolution.java
@@ -1,42 +1,57 @@
 
 public class backtrackingSolution {
-  // Print the board
-  void printBoard(boolean chessBoard[][]){
-    System.out.println("Chess Board:");
-    for (int i = 0; i < chessBoard.length; i++){
-      for (int j = 0; j < chessBoard[i].length; j++){
-        if (chessBoard[i][j] == true){
-          System.out.print("|_Q_");
-        }
-        else{
-          System.out.print("|___");
-        }
-      }
-      System.out.print("|\n");
+  public static boolean[][] solveNQueensWithBacktracking(int board_size){
+    // arrays for tracking queens' positions & bad areas
+    boolean chessBoard[][] = new boolean[board_size][board_size];
+    int usedRow[] = new int[board_size*2];
+    int downDiag[] = new int[board_size*2];
+    int upDiag[] = new int[board_size*2];
+
+    // start the recursion
+    if (continueBT(chessBoard, usedRow, downDiag, upDiag, 0, board_size) == true){
+      NQueensUtil.printBoard(chessBoard);
+      return chessBoard;
     }
+    else{
+      // Means a combination could not be found
+      System.out.println("Failed to create a solution");
+      return chessBoard;
+    }
+
   }
-
-  boolean checkPlacementValidity(boolean chessBoard[][], int sel_row, int sel_col){
-
-    // Checks if row is valid
-    for (int i = 0; i < sel_col; i++){
-      if (chessBoard[row][i] == true){
-        return false;
-      }
+  public static boolean continueBT(boolean chessBoard[][], int usedRow[], int downDiag[], int upDiag[], int sel_col, int board_size){
+    // If we have reached the board size, a solution has been found
+    if (sel_col == board_size){
+      return  true;
     }
 
-    // Checks upper diagonal
-    for (int i = 0; i < sel_row; i++)
-
+    // Iterate through the rows of this column and see if we can place a queen
+    for (int row = 0; row < board_size; row++){
+      // If found safe, place a queen
+      if(NQueensUtil.checkPlacementValidity(chessBoard, usedRow, downDiag, upDiag, row, sel_col, board_size) == true){
+        // Placing queen
+        NQueensUtil.placeQueen(chessBoard, usedRow, downDiag, upDiag, row, sel_col, board_size);
+        // Move on to next column, if that works return true
+        if (continueBT(chessBoard, usedRow, downDiag, upDiag, sel_col+1, board_size) == true){
+          return true;
+        }
+        // If we did not return before, that means we are backtracking:
+        //   Remove the queen placed now and try another row
+        NQueensUtil.removeQueen(chessBoard, usedRow, downDiag, upDiag, row, sel_col, board_size);
+      }
+    }
+    // Return false if nothing worked
+    return false;
   }
 
   public static void main(String args[])
     {
-      boolean chessBoard[][] = new boolean[5][5];
-      chessBoard[2][2] = true;
-      chessBoard[4][4] = true;
-      chessBoard[1][4] = true;
-      backtrackingSolution Solver = new backtrackingSolution();
-      Solver.printBoard(chessBoard);
+      for(int board_size = 1; board_size < 100; board_size++) {
+        long start_time = System.nanoTime();
+        boolean[][] chessBoard = solveNQueensWithBacktracking(board_size);
+        long end_time = System.nanoTime();
+        System.out.println("Backtracking Time(N = " + board_size + "): "+(end_time - start_time) + "ns");
+        NQueensUtil.validateSolution(chessBoard);
+      }
     }
 }
